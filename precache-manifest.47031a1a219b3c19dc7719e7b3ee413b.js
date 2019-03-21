20a27c99b935e8e3fe414128dd057e600cc2b632
Terence Chiu <tchiu2@gmail.com>
2019-03-21 00:04:49 -0400

    Updates

---
+++ precache-manifest.47031a1a219b3c19dc7719e7b3ee413b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69a4a94142e5535d384e",
+    "url": "/towers-of-hanoi/static/css/main.eb493302.chunk.css"
+  },
+  {
+    "revision": "69a4a94142e5535d384e",
+    "url": "/towers-of-hanoi/static/js/main.3703646c.chunk.js"
+  },
+  {
+    "revision": "3a47fbe81ac6e865ae6c",
+    "url": "/towers-of-hanoi/static/js/runtime~main.f93ba90f.js"
+  },
+  {
+    "revision": "4302a885d1fb170db828",
+    "url": "/towers-of-hanoi/static/js/2.3e9cd2f5.chunk.js"
+  },
+  {
+    "revision": "d5736297a706657f942accb3dac968e9",
+    "url": "/towers-of-hanoi/index.html"
+  }
+];
